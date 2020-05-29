63ee0a2ff8d5b8c4c8dff09f46b0b46c7505b7e0
paduraru2009 <ciprian.paduraru2009@gmail.com>
2020-05-29 09:18:01 +0300

    Check the readme

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -13,10 +13,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
 parser.add_argument('--num_samples', default=20, type=int)
 parser.add_argument('--dset_type', default='test', type=str)
+parser.add_argument('--external_test', type=bool, default=False)
+parser.add_argument('--external', type=bool, default=False)
 
 
 def get_generator(checkpoint):
     args = AttrDict(checkpoint['args'])
+    args['loader_num_workers'] = 0
     generator = TrajectoryGenerator(
         obs_len=args.obs_len,
         pred_len=args.pred_len,
@@ -36,8 +39,8 @@ def get_generator(checkpoint):
         grid_size=args.grid_size,
         batch_norm=args.batch_norm)
     generator.load_state_dict(checkpoint['g_state'])
-    generator.cuda()
-    generator.train()
+    generator.cpu() #.cuda()
+    generator.eval()
     return generator
 
 
@@ -60,7 +63,7 @@ def evaluate(args, loader, generator, num_samples):
     total_traj = 0
     with torch.no_grad():
         for batch in loader:
-            batch = [tensor.cuda() for tensor in batch]
+            batch = [tensor.cpu() for tensor in batch] #[tensor.cuda() for tensor in batch]
             (obs_traj, pred_traj_gt, obs_traj_rel, pred_traj_gt_rel,
              non_linear_ped, loss_mask, seq_start_end) = batch
 
@@ -105,13 +108,183 @@ def main(args):
         checkpoint = torch.load(path)
         generator = get_generator(checkpoint)
         _args = AttrDict(checkpoint['args'])
+        _args['loader_num_workers'] = 0
         path = get_dset_path(_args.dataset_name, args.dset_type)
         _, loader = data_loader(_args, path)
         ade, fde = evaluate(_args, loader, generator, args.num_samples)
         print('Dataset: {}, Pred Len: {}, ADE: {:.2f}, FDE: {:.2f}'.format(
             _args.dataset_name, _args.pred_len, ade, fde))
 
+# FLASK SESSION GLOBAL DEFINES
+import numpy as np
+from flask import Flask, jsonify, request
+import json
+import io
+app = Flask(__name__)
+
+NUM_FRAMES_TO_OBSERVE = 8
+NUM_FRAMES_TO_PREDICT = 8
+MAIN_AGENT_NAME = "main" # The agent under evaluation
+MAIN_AGENT_INDEX = 0
+generator = None
+history_pos = {} # History of positons for a given agent name
+#----------
+
+# This code runs the inference for one frome
+# Input params:
+# agentsObservedPos dict of ['agentName'] -> position as np array [2], all agents observed in this frame
+# optional: forcedHistoryDict -> same as above but with NUM_FRAMES_OBSERVED o neach agent, allows you to force / set history
+# Output : returns the position of the 'main' agent
+def DoModelInferenceForFrame(agentsObservedOnFrame, forcedHistoryDict = None):
+    global history_pos
+
+    # Update the history if forced param is used
+    if forcedHistoryDict != None:
+        for key, value in forcedHistoryDict.items():
+            assert isinstance(value, np.ndarray), "value is not instance of numpy array"
+            assert value.shape is not (NUM_FRAMES_TO_OBSERVE, 2)
+            history_pos[key] = value
+
+    # Update the history of agents seen with the new observed values
+    for key, value in agentsObservedOnFrame.items():
+        # If agent was not already in the history pos, init everything with local value
+        if key not in history_pos:
+            history_pos[key] = np.tile(value, [NUM_FRAMES_TO_OBSERVE, 1])
+        else:  # Else, just put his new pos in the end of history
+            values = history_pos[key]
+            values[0:NUM_FRAMES_TO_OBSERVE - 1] = values[1:NUM_FRAMES_TO_OBSERVE]
+            values[NUM_FRAMES_TO_OBSERVE - 1] = value
+
+    # Do simulation using the model
+    # ------------------------------------------
+
+    # Step 1: fill the input
+    numAgentsThisFrame = len(agentsObservedOnFrame)
+
+    # Absolute observed trajectories
+    obs_traj = np.zeros(shape=(NUM_FRAMES_TO_OBSERVE, numAgentsThisFrame, 2), dtype=np.float32)
+    # Zero index is main, others are following
+    obs_traj[:, MAIN_AGENT_INDEX, :] = history_pos[MAIN_AGENT_NAME]
+    index = 1
+    indexToAgentNameMapping = {}
+    indexToAgentNameMapping[MAIN_AGENT_INDEX] = MAIN_AGENT_NAME
+    for key, value in agentsObservedOnFrame.items():
+        if key != MAIN_AGENT_NAME:
+            obs_traj[:,index,:] = history_pos[key]
+            indexToAgentNameMapping[index] = key
+            index += 1
+
+    # Relative observed trajectories
+    obs_traj_rel = np.zeros(shape=(NUM_FRAMES_TO_OBSERVE, numAgentsThisFrame, 2), dtype=np.float32)
+    obs_traj_rel[1:, :, :] = obs_traj[1:, :, :] - obs_traj[:-1, :, :]
+
+    seq_start_end = np.array([[0, numAgentsThisFrame]])  # We have only 1 batch containing all agents
+    # Transform them to torch tensors
+    obs_traj = torch.from_numpy(obs_traj).type(torch.float)
+    obs_traj_rel = torch.from_numpy(obs_traj_rel).type(torch.float)
+    seq_start_end = torch.from_numpy(seq_start_end)
+
+    pred_traj_fake_rel = generator(obs_traj, obs_traj_rel, seq_start_end)
+    pred_traj_fake = relative_to_abs(pred_traj_fake_rel, obs_traj[-1])
+
+    # Take the first predicted position and add it to history
+    pred_traj_fake = pred_traj_fake.detach().numpy()
+    newMainAgentPos = pred_traj_fake[0][0]  # Agent 0 is our main agent
+
+    return newMainAgentPos
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method =='POST':
+        # Get the file from request
+        #file = request.files['file']
+        dataReceived = json.loads(request.data)
+
+        agentsObservedThisFrame = dataReceived['agentsPosThisFrame']
+        agentsForcedHistory = dataReceived['agentsForcedHistoryPos'] if 'agentsForcedHistoryPos' in dataReceived else None
+
+        # Read all agents data received
+        #-----------------------------------------
+        #agentIndex = 1
+        agentsObservedPos = {}
+        for key,value in agentsObservedThisFrame.items():
+            value = np.array(value, dtype=np.float32)
+            if key == MAIN_AGENT_NAME:
+                agentsObservedPos[MAIN_AGENT_NAME] = value
+            else:
+                agentsObservedPos[key] = value
+
+        forcedHistoryPos = None
+        if agentsForcedHistory is not None:
+            forcedHistoryPos = {}
+            for key, value in agentsForcedHistory.items():
+                value = np.array(value, dtype=np.float32)
+                if key == MAIN_AGENT_NAME:
+                    forcedHistoryPos[MAIN_AGENT_NAME] = value
+                else:
+                    forcedHistoryPos[key] = value
+
+        # Then do model inference for agents observed on this frame
+        # Get back the new position for main agent and return it to caller
+        newMainAgentPos = DoModelInferenceForFrame(agentsObservedPos, forcedHistoryPos)
+        return jsonify(newMainAgentPos = str(list(newMainAgentPos)))
+
+
+def deloyModelForFlaskInference():
+    checkpoint = torch.load(args.model_path)
+
+    global generator
+    generator = get_generator(checkpoint)
+    _args = AttrDict(checkpoint['args'])
+    _args['loader_num_workers'] = 0
+
+def startExternalTest():
+    deloyModelForFlaskInference()
+
+    historyMainAgent = np.zeros(shape=(NUM_FRAMES_TO_OBSERVE, 2))
+    agentsObservedPos = {MAIN_AGENT_NAME : np.array([0,0], dtype=np.float32)}
+
+    for frameIndex in range(100):
+        forcedHistory = None
+        if frameIndex == 0:
+            forcedHistory = {MAIN_AGENT_NAME : historyMainAgent}
+
+        newMainAgentPos = DoModelInferenceForFrame(agentsObservedPos, forcedHistory)
+        print(f"Frame {frameIndex}: {newMainAgentPos}")
+        agentsObservedPos[MAIN_AGENT_NAME] = newMainAgentPos
+
+"""
+    (obs_traj, pred_traj_gt, obs_traj_rel, pred_traj_gt_rel,
+     non_linear_ped, loss_mask, seq_start_end) = batch
+
+    ade, fde = [], []
+    total_traj += pred_traj_gt.size(1)
+
+    for _ in range(num_samples):
+        pred_traj_fake_rel = generator(
+            obs_traj, obs_traj_rel, seq_start_end
+        )
+        pred_traj_fake = relative_to_abs(
+            pred_traj_fake_rel, obs_traj[-1]
+        )
+        ade.append(displacement_error(
+            pred_traj_fake, pred_traj_gt, mode='raw'
+        ))
+        fde.append(final_displacement_error(
+            pred_traj_fake[-1], pred_traj_gt[-1], mode='raw'
+        ))
+"""
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args)
+    if True and args.external == True:
+        deloyModelForFlaskInference()
+        app.run()
+    elif args.external_test == True:
+        startExternalTest()
+    else: # normal evaluation
+        main(args)
+
+
+
--- sgan/models.py
+++ sgan/models.py
@@ -19,9 +19,9 @@ def make_mlp(dim_list, activation='relu', batch_norm=True, dropout=0):
 
 def get_noise(shape, noise_type):
     if noise_type == 'gaussian':
-        return torch.randn(*shape).cuda()
+        return torch.randn(*shape).cpu()#.cuda()
     elif noise_type == 'uniform':
-        return torch.rand(*shape).sub_(0.5).mul_(2.0).cuda()
+        return torch.rand(*shape).sub_(0.5).mul_(2.0).cpu()#.cuda()
     raise ValueError('Unrecognized noise type "%s"' % noise_type)
 
 
@@ -47,8 +47,8 @@ class Encoder(nn.Module):
 
     def init_hidden(self, batch):
         return (
-            torch.zeros(self.num_layers, batch, self.h_dim).cuda(),
-            torch.zeros(self.num_layers, batch, self.h_dim).cuda()
+            torch.zeros(self.num_layers, batch, self.h_dim).cpu(),#.cuda(),
+            torch.zeros(self.num_layers, batch, self.h_dim).cpu()#.cuda()
         )
 
     def forward(self, obs_traj):
@@ -60,8 +60,8 @@ class Encoder(nn.Module):
         """
         # Encode observed Trajectory
         batch = obs_traj.size(1)
-        obs_traj_embedding = self.spatial_embedding(obs_traj.view(-1, 2))
-        obs_traj_embedding = obs_traj_embedding.view(
+        obs_traj_embedding = self.spatial_embedding(obs_traj.reshape(-1, 2))
+        obs_traj_embedding = obs_traj_embedding.reshape(
             -1, batch, self.embedding_dim
         )
         state_tuple = self.init_hidden(batch)
@@ -528,7 +528,7 @@ class TrajectoryGenerator(nn.Module):
 
         decoder_c = torch.zeros(
             self.num_layers, batch, self.decoder_h_dim
-        ).cuda()
+        ).cpu()#cuda()
 
         state_tuple = (decoder_h, decoder_c)
         last_pos = obs_traj[-1]
